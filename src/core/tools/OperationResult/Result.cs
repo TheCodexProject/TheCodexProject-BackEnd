ffd40e1b1f485c502551d639737ae7115f719ab3
Mads Hylleberg Nielsen <102622934+MalasataXD@users.noreply.github.com>
2024-09-03 11:01:18 +0200

    Initial

---
+++ src/core/tools/OperationResult/Result.cs
@@ -0,0 +1,96 @@
+﻿namespace OperationResult;
+
+/// <summary>
+/// A class that represents the result of an operation without a return value.
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// The flag that indicates if the result is a failure.
+    /// (Default value is false)
+    /// </summary>
+    private bool  _isFailure;
+    
+    /// <summary>
+    /// The collection of errors that have occurred.
+    /// </summary>
+    private Exception[] _errors = [];
+
+    /// <summary>
+    /// Factory method to create a new instance of Result that is a success.
+    /// </summary>
+    /// <returns>A sucessful <see cref="Result"/></returns>
+    public static Result Success() => new();
+    
+    /// <summary>
+    /// Factory method to create a new instance of Result that is a failure.
+    /// </summary>
+    /// <param name="errors">An array of errors, that has happened.</param>
+    /// <returns>A failed <see cref="Result"/></returns>
+    public static Result Failure(params Exception[] errors) => new() { _isFailure = true, _errors = errors };
+    
+    /// <summary>
+    /// A flag that indicates if the result is a failure.
+    /// </summary>
+    public bool IsFailure => _isFailure;
+    
+    /// <summary>
+    /// The collection of errors that have occurred.
+    /// </summary>
+    public IEnumerable<Exception> Errors => _errors;
+}
+
+/// <summary>
+/// A class that represents the result of an operation with a return value.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Result<T>
+{
+    /// <summary>
+    /// The collection of errors that have occurred.
+    /// </summary>
+    private Exception[] _errors = [];
+    
+    /// <summary>
+    /// A value that is returned from the operation
+    /// </summary>
+    public T Value { get; private init; } = default!;
+    
+    /// <summary>
+    /// Implicit conversion operator to convert a T to a Result.
+    /// </summary>
+    /// <param name="value">The value to be returned.</param>
+    /// <returns>Returns a sucessful <see cref="Result"/> with a return false. </returns>
+    public static implicit operator Result<T>(T value) => Success(value);
+    
+    /// <summary>
+    /// Implicit conversion operator to convert a Result to a T.
+    /// </summary>
+    /// <param name="result">The result to be converted</param>
+    /// <returns>The value from within the <see cref="Result"/></returns>
+    public static implicit operator T(Result<T> result) => result.Value;
+
+    /// <summary>
+    /// Factory method to create a new instance of Result that is a success.
+    /// </summary>
+    /// <param name="value">The value to be returned.</param>
+    /// <returns>A sucessful <see cref="Result"/> with a return value.</returns>
+    public static Result<T> Success(T value) => new() { Value = value };
+    
+    /// <summary>
+    /// Factory method to create a new instance of Result that is a failure.
+    /// </summary>
+    /// <param name="errors">A collection of errors that has occured.</param>
+    /// <returns>A failed <see cref="Result"/></returns>
+    public static Result<T> Failure(params Exception[] errors) => new() { IsFailure = true, _errors = errors };
+    
+    /// <summary>
+    /// A flag that indicates if the result is a failure.
+    /// </summary>
+    public bool IsFailure { get; private set; }
+
+    /// <summary>
+    /// The collection of errors that have occurred.
+    /// </summary>
+    public IEnumerable<Exception> Errors => _errors;
+}
---
+++ src/tests/Unit/Tools/OperationResult/ResultTests.cs
@@ -0,0 +1,229 @@
+﻿using OperationResult;
+
+namespace unittesting.Tools.OperationResult;
+
+public class ResultTests
+{
+   public const string ErrorMessage = "An error has occurred.";
+   
+   /// <summary>
+   /// Test to assert that a successful result is not a failure.
+   /// </summary>
+   [Fact]
+   [Trait("Result (No return value)","Success")]
+   public void Success_Result_IsFailure_Should_Be_False()
+   {
+      // Arrange
+      var result = Result.Success();
+      
+      // Act
+      var isFailure = result.IsFailure;
+      
+      // Assert
+      Assert.False(isFailure);
+   }
+   
+   /// <summary>
+   /// Test to assert that a failed result is a failure.
+   /// </summary>
+   [Fact]
+   [Trait("Result (No return value)","Failure")]
+   public void Failure_Result_IsFailure_Should_Be_True()
+   {
+      // Arrange
+      var result = Result.Failure(new Exception(ErrorMessage));
+      
+      // Act
+      var isFailure = result.IsFailure;
+      
+      // Assert
+      Assert.True(isFailure);
+   }
+   
+   /// <summary>
+   /// Test to assert that a failed result contains an error message.
+   /// </summary>
+   [Fact]
+   [Trait("Result (No return value)","Failure")]
+   public void Failure_Result_Errors_Should_Contain_Error_Message()
+   {
+      // Arrange
+      var result = Result.Failure(new Exception(ErrorMessage));
+      
+      // Act
+      var errors = result.Errors;
+      
+      // Assert
+      Assert.Contains(ErrorMessage, errors.Select(e => e.Message));
+   }
+   
+   /// <summary>
+   /// Test to assert that a failed result can contain different error messages.
+   /// </summary>
+   [Fact]
+   [Trait("Result (No return value)","Failure")]
+   public void Failure_Result_Errors_Should_Contain_All_Error_Messages()
+   {
+      // Arrange
+      var errorMessages = new[] { "Error 1", "Error 2", "Error 3" };
+      var result = Result.Failure(errorMessages.Select(e => new Exception(e)).ToArray());
+      
+      // Act
+      var errors = result.Errors;
+      
+      // Assert
+      var exceptions = errors.ToList();
+      Assert.Equal(errorMessages.Length, exceptions.Count());
+      Assert.All(errorMessages, em => Assert.Contains(em, exceptions.Select(e => e.Message)));
+   }
+
+   /// <summary>
+   /// Test to assert that a failed result can contain different exception types.
+   /// </summary>
+   [Fact]
+   [Trait("Result (No return value)","Failure")]
+   public void Failure_Result_Errors_Can_Be_Different_Exception_Types()
+   {
+      // Arrange
+      var errors = new Exception[] { new ArgumentException(), new InvalidOperationException(), new ArgumentNullException() };
+      var result = Result.Failure(errors);
+      
+      // Act
+      var resultErrors = result.Errors;
+      
+      // Assert
+      var exceptions = resultErrors.ToList();
+      Assert.Equal(errors.Length, exceptions.Count());
+      Assert.All(errors, e => Assert.Contains(e, exceptions));
+   }
+}
+
+public class ResultOfTTests
+{
+   public const string ErrorMessage = "An error has occurred.";
+   public const int Value = 10;
+   
+   [Fact]
+   public void Success_Result_Create_Result_With_Value()
+   {
+      // Arrange
+      var value = Value;
+      
+      // Act
+      var result = Result<int>.Success(value);
+      
+      // Assert
+      Assert.Equal(value, result.Value);
+   }
+   
+   [Fact]
+   public void Success_Result_Create_Result_With_Value_IsFailure_Should_Be_False()
+   {
+      // Arrange
+      var value = Value;
+      
+      // Act
+      var result = Result<int>.Success(value);
+      
+      // Assert
+      Assert.False(result.IsFailure);
+   }
+   
+   [Fact]
+   public void Failure_Result_Create_Result_With_Error_Message_IsFailure_Should_Be_True()
+   {
+      // Arrange
+      var result = Result<int>.Failure(new Exception(ErrorMessage));
+      
+      // Act
+      var isFailure = result.IsFailure;
+      
+      // Assert
+      Assert.True(isFailure);
+   }
+   
+   [Fact]
+   public void Failure_Result_Create_Result_With_Error_Message_Errors_Should_Contain_Error_Message()
+   {
+      // Arrange
+      var result = Result<int>.Failure(new Exception(ErrorMessage));
+      
+      // Act
+      var errors = result.Errors;
+      
+      // Assert
+      Assert.Contains(ErrorMessage, errors.Select(e => e.Message));
+   }
+   
+   [Fact]
+   public void Failure_Result_Create_Result_With_Error_Message_Errors_Should_Contain_All_Error_Messages()
+   {
+      // Arrange
+      var errorMessages = new[] { "Error 1", "Error 2", "Error 3" };
+      var result = Result<int>.Failure(errorMessages.Select(e => new Exception(e)).ToArray());
+      
+      // Act
+      var errors = result.Errors;
+      
+      // Assert
+      var exceptions = errors.ToList();
+      Assert.Equal(errorMessages.Length, exceptions.Count());
+      Assert.All(errorMessages, em => Assert.Contains(em, exceptions.Select(e => e.Message)));
+   }
+   
+   [Fact]
+   public void Failure_Result_Create_Result_With_Error_Message_Errors_Can_Be_Different_Exception_Types()
+   {
+      // Arrange
+      var errors = new Exception[] { new ArgumentException(), new InvalidOperationException(), new ArgumentNullException() };
+      var result = Result<int>.Failure(errors);
+      
+      // Act
+      var resultErrors = result.Errors;
+      
+      // Assert
+      var exceptions = resultErrors.ToList();
+      Assert.Equal(errors.Length, exceptions.Count());
+      Assert.All(errors, e => Assert.Contains(e, exceptions));
+   }
+   
+   [Fact]
+   public void Implicit_Conversion_From_T_To_Result_T()
+   {
+      // Arrange
+      var value = Value;
+      
+      // Act
+      Result<int> result = value;
+      
+      // Assert
+      Assert.Equal(value, result.Value);
+   }
+   
+   [Fact]
+   public void Implicit_Conversion_From_Result_T_To_T()
+   {
+      // Arrange
+      var value = Value;
+      var result = Result<int>.Success(value);
+      
+      // Act
+      int resultValue = result;
+      
+      // Assert
+      Assert.Equal(value, resultValue);
+   }
+   
+   [Fact]
+   public void Implicit_Conversion_From_Result_T_To_T_With_Failed_Result()
+   {
+      // Arrange
+      var result = Result<int>.Failure(new Exception(ErrorMessage));
+      
+      // Act
+      int resultValue = result;
+      
+      // Assert
+      Assert.Equal(default(int), resultValue);
+   }
+}
