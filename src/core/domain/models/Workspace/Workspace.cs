6b2a7a3e235f82415904671586a35d1cd111aa46
BjarkeBP <Bjarkebp@live.dk>
2024-09-15 18:07:15 +0200

    Implemented resource interface and enhanced Workspace model logic
    Implemented a resource interface and added new methods to the Workspace model: UpdateTitle, AddResource, RemoveResource, and Create. This section is now ready for testing.

--- src/core/domain/models/Workspace/Workspace.cs
+++ src/core/domain/models/Workspace/Workspace.cs
@@ -1,17 +1,102 @@
 ﻿
-
-
+using domain.models.Interfaces;
 using domain.models.Workspace.values;
+using OperationResult;
 
 namespace domain.models.Workspace;
 public class Workspace
 {
-    // Does this need to inculde groups
-    public Guid Id { get; set; }
 
-    public WorkspaceTitle Title { get; set; }
+    #region properties
 
-    // Todo add Project / items here 
-    // public List<Project> Projects { get; set; }
+    /// <summary>
+    /// The unique identifier for the WorkItem.
+    /// </summary>
+    public Guid Id { get; private set; }
+
+    /// <summary>
+    /// Holds the title of the Workspace.
+    /// </summary>
+    public WorkspaceTitle? Title { get; private set; }
+
+    /// <summary>
+    /// Holds a list of resources.
+    /// </summary>
+    public List<IResource> Resources { get; private set; }
+
+    #endregion
+
+    #region constructors
+
+    /// <summary>
+    /// Creates a new instance of <see cref="Workspace"/> with default values.
+    /// </summary>
+    private Workspace()
+    {
+        Id = Guid.NewGuid();
+        Resources = new List<IResource>();
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="WorkItem"/> with default values.
+    /// </summary>
+    /// <returns>A <see cref="Workspace"/></returns>
+    public static Workspace Create()
+    {
+        return new Workspace();
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Updates the title of the WorkItem.
+    /// </summary>
+    /// <param name="title">The new title.</param>
+    /// <returns>A <see cref="Result"/> indicating if the update was a success.</returns>
+    public Result UpdateTitle(string title)
+    {
+        var newTitle = WorkspaceTitle.Create(title);
+
+        // ! VALIDATION
+        // Are there any specific things that we would like to validate, when the user updates the title?
+        if (newTitle.IsFailure)
+        {
+            // ! Return the errors from the result.
+            return Result.Failure(newTitle.Errors.ToArray());
+        }
+
+        // Update the title.
+        Title = newTitle.Value;
+
+        return Result.Success();
+    }
+
+    /// <summary>
+    /// Adds a resource to resources
+    /// </summary>
+    /// <param name="resource">The resource to add.</param>
+    /// <returns>A <see cref="Result"/> indicating if the update was a success.</returns>
+    public Result AddResource(IResource resource)
+    {
+        Resources.Add(resource);
+        return Result.Success();
+    }
+
+    /// <summary>
+    /// Removes a resource from resources
+    /// </summary>
+    /// <param name="resource">The resource to remove.</param>
+    /// <returns>A <see cref="Result"/> indicating if the update was a success.</returns>
+    public Result RemoveResource(IResource resource)
+    {
+        var test = Resources.Remove(resource);
+        
+        if (!test)
+        {
+            return Result.Failure();
+        }
+
+        return Result.Success();
+    }
 }
 
