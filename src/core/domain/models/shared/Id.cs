3132f8be73c38a68e730f1ec9852e61ad817b222
Mads Hylleberg Nielsen <102622934+MalasataXD@users.noreply.github.com>
2024-09-16 15:07:42 +0200

    Minor Tweaks to the User and a new Id class.
    - Minor tweaks to the user including making all fields nullable. Which allows for creating a new "empty" user. + Removed FullName property, since that can be created by combining the First and Last name of the user.
    - Made the User adopt the Id generic type to handle its Id.

--- src/core/domain/models/shared/Id.cs
+++ src/core/domain/models/shared/Id.cs
@@ -1,13 +1,58 @@
+using domain.exceptions.Id;
+using OperationResult;
+
 namespace domain.models.shared;
 
 public class Id<T>
 {
-    public Guid Value { get; private protected set; }
+    /// <summary>
+    /// The unique identifier of the entity.
+    /// </summary>
+    public Guid Value { get; private set; }
     
-    protected Id()
+    /// <summary>
+    /// The default constructor of the <see cref="Id{T}"/> class.
+    /// </summary>
+    private Id()
     {
-        Value = Guid.NewGuid(); 
+        Value = Guid.NewGuid();
     }
     
+    /// <summary>
+    /// The factory method to create a new instance of the <see cref="Id{T}"/> class.
+    /// </summary>
+    /// <returns></returns>
+    public static Id<T> Create()
+    {
+        return new Id<T>();
+    }
+
+    /// <summary>
+    /// A factory method to create a new instance of the <see cref="Id{T}"/> class from a string.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public static Result<Id<T>> FromString(string id)
+    {
+        if(Guid.TryParse(id, out var guid))
+        {
+            return Result<Id<T>>.Success(new Id<T> { Value = guid });
+        }
+        
+        return Result<Id<T>>.Failure(new IdConversionExpection());
+    }
+    
+    /// <summary>
+    /// A factory method to create a new instance of the <see cref="Id{T}"/> class from a <see cref="Guid"/>.
+    /// </summary>
+    /// <param name="guid"></param>
+    /// <returns></returns>
+    public static Id<T> FromGuid(Guid guid) => new() { Value = guid };
+    
+    /// <summary>
+    /// Implicitly converts the <see cref="Id{T}"/> to a <see cref="Guid"/>.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public static implicit operator Guid(Id<T> id) => id.Value;
 }
